669072dfe471f11e6705e9eae07e4483334e4086
Shal <shal.work@mail.ru>
2022-07-24 11:11:56 +0300

    30-th commit, changed water ripples

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,17 +10,25 @@ $('.selector-ripples').ripples({
     $('.selector-ripples-auto').ripples({
         resolution: 256,
         dropRadius: 20,
-        perturbance: 0.04,
-        interactive: false,
-
+        perturbance: 0.14,
+        // interactive: false,
     });	
+    let $el1 = $('.selector-ripples-auto');
+    let x1 = Math.random() * $el1.outerWidth();
+    let y1 = Math.random() * $el1.outerHeight();
+    console.log(x1);
+    console.log(y1);
+    let dropRadius1 = 20;
+    let strength1 = 0.08 + Math.random() * 0.08;
+    console.log(strength1);
+    $el1.ripples('drop', x1, y1, dropRadius1, strength1);
+
     setInterval(function() {
-        var $el = $('.selector-ripples-auto');
-        var x = Math.random() * $el.outerWidth();
-        var y = Math.random() * $el.outerHeight();
-        var dropRadius = 15;
-        // var strength = 0.08 + Math.random() * 0.8;
-        var strength = 0.18;
+        let $el = $('.selector-ripples-auto');
+        let x = Math.random() * $el.outerWidth();
+        let y = Math.random() * $el.outerHeight();
+        let dropRadius = 15;
+        let strength = 0.08 + Math.random() * 0.8;
         $el.ripples('drop', x, y, dropRadius, strength);
-    }, 400);
+    }, 10000);
 // });
