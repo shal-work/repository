7028b89e38fdb035f0f75097ec0e8dda4ba564b2
Shal <shal.work@mail.ru>
2023-01-22 17:13:04 +0300

    46-01-th commit, add IT-KAMASUTRA

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,16 +36,16 @@ $('.selector-ripples').ripples({
 
 const layer = document.querySelectorAll('.selector');
 layer.forEach(item => {
-    // console.log('width: ', item.offsetWidth );
-    // console.log('offsetHeight: ', item.offsetHeight );
+    console.log('width: ', item.offsetWidth );
+    console.log('offsetHeight: ', item.offsetHeight );
     item.addEventListener('mousemove', e => {
         // let mouseX = e.pageX - e.target.offsetLeft;
         // let mouseY = e.pageY - e.target.offsetTop;
         let layerX = e.layerX - item.offsetWidth/2;
         let layerY = e.layerY - item.offsetHeight/2;
         item.style.transform = 'translate(' + layerX / -10 + 'px ,' + layerY / 20 + 'px)' + ' ' + 'scale(' +1.05 +')';
-        // console.log('layerX: ', e.layerX);
-		// console.log('layerY: ', e.layerY);
+        console.log('layerX: ', e.layerX);
+		console.log('layerY: ', e.layerY);
     });
     item.addEventListener('mouseout', e => {
         item.style.transform = 'translate(0px , 0px)' + ' ' + 'scale(' + 1 +')';
