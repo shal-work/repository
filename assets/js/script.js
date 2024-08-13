89acd1dace926345bed80ec8c0e67212348ef58b
Shal <shal.work@mail.ru>
2024-08-13 09:31:54 +0300

    69-th commit, add site figma Crypto_Brains

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,31 +6,31 @@ $('.selector-ripples').ripples({
 });
 
 // $(document).ready(function() {
-	//для автоматического булькания 
-    $('.selector-ripples-auto').ripples({
-        resolution: 256,
-        dropRadius: 20,
-        perturbance: 0.14,
-        // interactive: false,
-    });	
-    let $el1 = $('.selector-ripples-auto');
-    let x1 = Math.random() * $el1.outerWidth();
-    let y1 = Math.random() * $el1.outerHeight();
-    // console.log(x1);
-    // console.log(y1);
-    let dropRadius1 = 20;
-    let strength1 = 0.08 + Math.random() * 0.08;
-    // console.log(strength1);
-    $el1.ripples('drop', x1, y1, dropRadius1, strength1);
+	//для автоматического булькания
+    // $('.selector-ripples-auto').ripples({
+    //     resolution: 256,
+    //     dropRadius: 20,
+    //     perturbance: 0.14,
+    //     // interactive: false,
+    // });
+    // let $el1 = $('.selector-ripples-auto');
+    // let x1 = Math.random() * $el1.outerWidth();
+    // let y1 = Math.random() * $el1.outerHeight();
+    // // console.log(x1);
+    // // console.log(y1);
+    // let dropRadius1 = 20;
+    // let strength1 = 0.08 + Math.random() * 0.08;
+    // // console.log(strength1);
+    // $el1.ripples('drop', x1, y1, dropRadius1, strength1);
 
-    setInterval(function() {
-        let $el = $('.selector-ripples-auto');
-        let x = Math.random() * $el.outerWidth();
-        let y = Math.random() * $el.outerHeight();
-        let dropRadius = 15;
-        let strength = 0.08 + Math.random() * 0.8;
-        $el.ripples('drop', x, y, dropRadius, strength);
-    }, 10000);
+    // setInterval(function() {
+    //     let $el = $('.selector-ripples-auto');
+    //     let x = Math.random() * $el.outerWidth();
+    //     let y = Math.random() * $el.outerHeight();
+    //     let dropRadius = 15;
+    //     let strength = 0.08 + Math.random() * 0.8;
+    //     $el.ripples('drop', x, y, dropRadius, strength);
+    // }, 10000);
 // });
 
 
@@ -44,7 +44,7 @@ layer.forEach(item => {
         // let mouseX = e.pageX - e.target.offsetLeft;
         // let mouseY = e.pageY - e.target.offsetTop;
 
-        // layerX/layerY - устарели надо e.offsetX / e.offsetY 
+        // layerX/layerY - устарели надо e.offsetX / e.offsetY
         let layerX = e.layerX - item.offsetWidth/2;
         let layerY = e.layerY - item.offsetHeight/2;
         item.style.transform = 'translate(' + layerX / -10 + 'px ,' + layerY / 20 + 'px)' + ' ' + 'scale(' +1.05 +')';
@@ -58,4 +58,3 @@ layer.forEach(item => {
 
     });
 });
-
